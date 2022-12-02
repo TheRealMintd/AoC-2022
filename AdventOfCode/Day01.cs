ee8156e68674cabfaad97f7ca2634524d3886b92
Wong Yi Xiong <johnmax2468@gmail.com>
2022-12-02 15:11:34 +0800

    Complete Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,26 @@
-﻿namespace AdventOfCode;
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+	private readonly int[][] _input;
 
-    public Day01()
-    {
-        _input = File.ReadAllText(InputFilePath);
-    }
+	public Day01()
+	{
+		string text = File.ReadAllText(InputFilePath);
+		string[] inventories = text.Split("\n\n");
+		_input = inventories.Select(inv => inv.Trim().Split('\n').Select(Int32.Parse).ToArray())
+			.ToArray();
+	}
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+	public override ValueTask<string> Solve_1()
+	{
+		int result = _input.Select(inv => inv.Sum()).Max();
+		return new(result.ToString());
+	}
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+	public override ValueTask<string> Solve_2()
+	{
+		int result = _input.Select(inv => inv.Sum()).OrderDescending().Take(3).Sum();
+		return new(result.ToString());
+	}
 }
